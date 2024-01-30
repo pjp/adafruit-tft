1d378003daa127ed236a723ea94b14f85b0f8613
pjp <paul@pearceful.net>
2024-01-30 16:58:35 +0000

    Added a time stamp

--- stats.py
+++ stats.py
@@ -41,6 +41,7 @@ height = disp.width  # we swap height/width to rotate it to landscape!
 width = disp.height
 image = Image.new("RGB", (width, height))
 rotation = 0
+LineSpacing = 5
 
 # Get drawing object to draw on image.
 draw = ImageDraw.Draw(image)
@@ -75,25 +76,42 @@ while True:
     # https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
     cmd = "hostname -I | cut -d' ' -f1"
     IP = "IP: " + subprocess.check_output(cmd, shell=True).decode("utf-8")
+
     cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
     CPU = subprocess.check_output(cmd, shell=True).decode("utf-8")
+
     cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%s MB\", $3,$2 }'"
     MemUsage = subprocess.check_output(cmd, shell=True).decode("utf-8")
+
     cmd = 'df -h | awk \'$NF=="/"{printf "Disk: %d/%d GB  %s", $3,$2,$5}\''
     Disk = subprocess.check_output(cmd, shell=True).decode("utf-8")
+
     cmd = "cat /sys/class/thermal/thermal_zone0/temp |  awk '{printf \"CPU Temp: %.1f C\", $(NF-0) / 1000}'"  # pylint: disable=line-too-long
     Temp = subprocess.check_output(cmd, shell=True).decode("utf-8")
 
-    # Write four lines of text.
+    cmd="date '+%Y%m%d:%H%M%S'"
+    Time = subprocess.check_output(cmd, shell=True).decode("utf-8")
+
+    # Write five lines of text.
     y = top
+    draw.text((x, y), Time, font=font, fill="#FFFF00")
+
+    # Extra spaving
+    y += (2 * LineSpacing)
+
+    y += (LineSpacing + font.getsize(Time)[1])
     draw.text((x, y), IP, font=font, fill="#FFFFFF")
-    y += font.getsize(IP)[1]
+
+    y += (LineSpacing + font.getsize(IP)[1])
     draw.text((x, y), CPU, font=font, fill="#FFFF00")
-    y += font.getsize(CPU)[1]
+
+    y += (LineSpacing + font.getsize(CPU)[1])
     draw.text((x, y), MemUsage, font=font, fill="#00FF00")
-    y += font.getsize(MemUsage)[1]
+
+    y += (LineSpacing + font.getsize(MemUsage)[1])
     draw.text((x, y), Disk, font=font, fill="#0000FF")
-    y += font.getsize(Disk)[1]
+
+    y += (LineSpacing + font.getsize(Disk)[1])
     draw.text((x, y), Temp, font=font, fill="#FF00FF")
 
     # Display image.
