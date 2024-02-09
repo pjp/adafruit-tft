4fb1e50331a37a34c111e45fe06e6dce33b61724
pjp <paul@pearceful.net>
2024-02-09 09:10:56 +0000

    Added path prefix

--- bin/get-cpu-temp-history-from-prometheus.sh
+++ bin/get-cpu-temp-history-from-prometheus.sh
@@ -6,4 +6,4 @@ echo "########################"
 echo "# History of CPU temp. from $RANGE minute(s) back"
 echo "########################"
 
-curl -s  "http://localhost:9090/api/v1/query?query=node_thermal_zone_temp\[${RANGE}m\]" | jq -r '.data.result[0].values| map(.[1])' | tr -d '[],"' | grep -v '^$' | awk '{printf "%3.1f\n", $1}' | ./calc-stats.sh "node_thermal_zone_temp deg. C"
+curl -s  "http://localhost:9090/api/v1/query?query=node_thermal_zone_temp\[${RANGE}m\]" | jq -r '.data.result[0].values| map(.[1])' | tr -d '[],"' | grep -v '^$' | awk '{printf "%3.1f\n", $1}' | ~/bin/calc-stats.sh "node_thermal_zone_temp deg. C"
--- bin/get-speed-history-summaries-from-prometheus.sh
+++ bin/get-speed-history-summaries-from-prometheus.sh
@@ -5,7 +5,6 @@ RANGE="${1:-6}"
 echo "########################"
 echo "# History of download/upload speeds from $RANGE hours back"
 echo "########################"
-# ./get-speed-histories-from-prometheus.sh 12 | grep "Download" | grep -iv '^#' | awk '{ print $2}' | tr -d 'Mb/s'  | ./calc-stats.sh
-curl -s  "http://localhost:9090/api/v1/query?query=speedtest_download_bits_per_second\[${RANGE}h\]" | jq -r '.data.result[0].values| map(.[1])' | tr -d '[],"' | grep -v '^$' | awk '{printf "%4.0f\n", $1/1000000}' | ./calc-stats.sh "speedtest_download Mb/s"
+curl -s  "http://localhost:9090/api/v1/query?query=speedtest_download_bits_per_second\[${RANGE}h\]" | jq -r '.data.result[0].values| map(.[1])' | tr -d '[],"' | grep -v '^$' | awk '{printf "%4.0f\n", $1/1000000}' | ~/bin/calc-stats.sh "speedtest_download Mb/s"
 
-curl -s  "http://localhost:9090/api/v1/query?query=speedtest_upload_bits_per_second\[${RANGE}h\]" | jq -r '.data.result[0].values| map(.[1])' | tr -d '[],"' | grep -v '^$'   | awk '{printf "%4.0f\n", $1/1000000}' | ./calc-stats.sh "speedtest_upload Mb/s"
+curl -s  "http://localhost:9090/api/v1/query?query=speedtest_upload_bits_per_second\[${RANGE}h\]" | jq -r '.data.result[0].values| map(.[1])' | tr -d '[],"' | grep -v '^$'   | awk '{printf "%4.0f\n", $1/1000000}' | ~/bin/calc-stats.sh "speedtest_upload Mb/s"
