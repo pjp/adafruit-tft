8409ab6cc8ccb6210d8a5346d670ce8e21104e8e
pjp <paul@pearceful.net>
2024-02-11 18:18:16 +0000

    Changed headings

--- bin/get-cpu-temp-history-from-prometheus.sh
+++ bin/get-cpu-temp-history-from-prometheus.sh
@@ -3,11 +3,9 @@
 RANGE="${1:-60m}"
 
 echo "########################"
-echo "# History of CPU temp."
+echo "# Stats. of CPU temp."
 echo "# over the last $RANGE"
 echo "########################"
-
-# curl -s  "http://localhost:9090/api/v1/query?query=node_thermal_zone_temp\[${RANGE}m\]" | jq -r '.data.result[0].values| map(.[1])' | tr -d '[],"' | grep -v '^$' | awk '{printf "%3.1f\n", $1}' | ~/bin/calc-stats.sh "node_thermal_zone_temp deg. C"
 
 source $HOME/bin/prometheus-lib.sh
 
--- bin/get-speed-history-summaries-from-prometheus.sh
+++ bin/get-speed-history-summaries-from-prometheus.sh
@@ -3,13 +3,9 @@
 RANGE="${1:-6h}"
 
 echo "########################"
-echo "# History of download/upload speeds"
+echo "# Stats. of download/upload speeds"
 echo "# over the last $RANGE"
 echo "########################"
-#curl -s  "http://localhost:9090/api/v1/query?query=speedtest_download_bits_per_second\[${RANGE}h\]" | jq -r '.data.result[0].values| map(.[1])' | tr -d '[],"' | grep -v '^$' | awk '{printf "%4.0f\n", $1/1000000}' | ~/bin/calc-stats.sh "speedtest_download Mb/s"
-
-#curl -s  "http://localhost:9090/api/v1/query?query=speedtest_upload_bits_per_second\[${RANGE}h\]" | jq -r '.data.result[0].values| map(.[1])' | tr -d '[],"' | grep -v '^$'   | awk '{printf "%4.0f\n", $1/1000000}' | ~/bin/calc-stats.sh "speedtest_upload Mb/s"
-
 
 source $HOME/bin/prometheus-lib.sh
 
