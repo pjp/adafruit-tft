26d7efd8eda2c9431b863130d1a95e243d22133a
pjp <paul@pearceful.net>
2024-02-11 09:08:07 +0000

    Sort out the range values
    Now can change/pass the range value from the mail script

--- bin/get-cpu-temp-history-from-prometheus.sh
+++ bin/get-cpu-temp-history-from-prometheus.sh
@@ -1,11 +1,14 @@
-#|/bin/bash
+#!/usr/bin/bash
 
-RANGE="${1:-60}"
+RANGE="${1:-60m}"
 
 echo "########################"
-echo "# History of CPU temp. from $RANGE minute(s) back"
+echo "# History of CPU temp."
+echo "# over the last $RANGE"
 echo "########################"
 
 # curl -s  "http://localhost:9090/api/v1/query?query=node_thermal_zone_temp\[${RANGE}m\]" | jq -r '.data.result[0].values| map(.[1])' | tr -d '[],"' | grep -v '^$' | awk '{printf "%3.1f\n", $1}' | ~/bin/calc-stats.sh "node_thermal_zone_temp deg. C"
 
-(source ~/bin/prometheus-lib.sh ; prometheus-lib-get-stats-from-list "$(prometheus-lib-get-history-as-list node_thermal_zone_temp ${RANGE}m)" "node_thermal_zone_temp deg. C")
+source $HOME/bin/prometheus-lib.sh
+
+prometheus-lib-get-stats-from-list "$(prometheus-lib-get-history-as-list node_thermal_zone_temp ${RANGE})" "node_thermal_zone_temp deg. C"
--- bin/get-speed-histories-from-prometheus.sh
+++ bin/get-speed-histories-from-prometheus.sh
@@ -1,4 +1,4 @@
-#|/bin/bash
+#!/usr/bin/bash
 
 RANGE="${1:-6}"
 
