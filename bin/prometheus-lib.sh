9d10181602282c95884f3d6bf4a4fb6401ed9c07
pjp <paul@pearceful.net>
2024-02-12 12:49:41 +0000

    Added help function with examples
    Added debug capability

--- bin/prometheus-lib.sh
+++ bin/prometheus-lib.sh
@@ -7,7 +7,7 @@
 #######################################
 # Designed to be referenced in scipts as 
 #
-# source ~/bin/Prometyheus-lib.sh
+# source ~/bin/Prometheus-lib.sh
 #
 #
 # These functions can also be called directly on the command line
@@ -17,42 +17,70 @@
 #    speedtest_download_bits_per_second 
 #    speedtest_upload_bits_per_second 
 #
-##############################################
-# (source ~/bin/prometheus-lib.sh ; prometheus-lib-get-latest-value node_thermal_zone_temp 1m)
-#
-# (source ~/bin/prometheus-lib.sh ; prometheus-lib-get-history-as-list node_thermal_zone_temp 1m)
-#
-# (source ~/bin/prometheus-lib.sh ; prometheus-lib-get-stats-from-list "$(prometheus-lib-get-history-as-list node_thermal_zone_temp 1h)" "History")
-#
-#
-###############################################
-# Need to massage the output to Mb/s using awk.
-#
-# Since the upload/download speed is only 
-# captured once an hour, we need to go back a 
-# few hours.
-#
-# (source ~/bin/prometheus-lib.sh ; prometheus-lib-get-latest-value speedtest_download_bits_per_second 1h | awk '{printf "%4.0f\n", $1/1000000}')
-#
-# (source ~/bin/prometheus-lib.sh ; prometheus-lib-get-history-as-list speedtest_download_bits_per_second 6h | awk '{printf "%4.0f\n", $1/1000000}')
-#
-# (source ~/bin/prometheus-lib.sh ; prometheus-lib-get-stats-from-list "$(prometheus-lib-get-history-as-list speedtest_download_bits_per_second 6h | awk '{printf "%4.0f\n", $1/1000000}')" "History")
-#
-#############################################################
-#
+export PROMETHEUS_IAM="prometheus-lib.sh"
 export PROMETHEUS_API="http://localhost:9090/api/v1"
+
+#################
+function prometheus-lib-help() {
+#################
+   echo -e "Example usage of functions in this library :-\n\n"
+
+   echo "(source ~/bin/$PROMETHEUS_IAM ; prometheus-lib-help)"
+   echo ""
+   echo "(source ~/bin/$PROMETHEUS_IAM ; prometheus-lib-get-latest-value node_thermal_zone_temp 1m)"
+   echo "(source ~/bin/$PROMETHEUS_IAM ; prometheus-lib-get-history-as-list node_thermal_zone_temp 1m)"
+   echo "(source ~/bin/$PROMETHEUS_IAM ; prometheus-lib-get-stats-from-list \"\$(prometheus-lib-get-history-as-list node_thermal_zone_temp 1h)\" "History")"
+   echo ""
+   echo ""
+   echo "For the following examples, we need to massage the output to Mb/s using awk."
+   echo "Since the upload/download speed is only captured once an hour, we need to go back a"
+   echo "few hours."
+   echo ""
+   echo "(source ~/bin/$PROMETHEUS_IAM ; prometheus-lib-get-latest-value speedtest_download_bits_per_second 1h | awk '{printf \"%4.0f\\n\", \$1/1000000}')"
+   echo "(source ~/bin/$PROMETHEUS_IAM ; prometheus-lib-get-history-as-list speedtest_download_bits_per_second 6h | awk '{printf \"%4.0f\\n\", \$1/1000000}')"
+   echo "(source ~/bin/$PROMETHEUS_IAM ; prometheus-lib-get-stats-from-list \"\$(prometheus-lib-get-history-as-list speedtest_download_bits_per_second 6h | awk '{printf \"%4.0f\\n\", \$1/1000000}')\" \"History\")"
+}
+
+#################
+function prometheus-lib() {
+#################
+   prometheus-lib-help
+} 
+
+##################
+function prometheus-lib() {
+##################
+   label="$1"
+   data="$2"
+
+   if [ ! -z "$PROMETHEUS_LIB_DEBUG" ]
+   then
+      echo  "${label}"
+      echo  "${data}"
+   fi
+}
 
 ##########################
 function prometheus-lib-get-history-as-list() {
 ##########################
    local metric="$1"
    local range="$2"
+   local curl_output=""
+   local jq_output=""
 
    if [ $# -lt 2 ]
    then
       echo "Too few parameters, need metric & range" >&2
    else
-      curl -s  "${PROMETHEUS_API}/query?query=${metric}\[${range}\]" | jq -r '.data.result[0].values| map(.[1])' | tr -d '[],"' | grep -v '^$' | awk '{printf "%3.1f\n", $1}'
+      # curl -s  "${H/30 * * * *API}/query?query=${metric}\[${range}\]" | jq -r '.data.result[0].values| map(.[1])' | tr -d '[],"' | grep -v '^$' | awk '{printf "%3.1f\n", $1}'
+
+      curl_output=$(curl -s "${PROMETHEUS_API}/query?query=${metric}\[${range}\]")
+      prometheus-lib "curl_output" "$curl_output"
+
+      jq_output="$(echo $curl_output | jq -r '.data.result[0].values| map(.[1])')"
+      prometheus-lib "jq_output" "$jq_output"
+
+      echo "$jq_output" | tr -d '[],"' | grep -v '^$' | awk '{printf "%3.1f\n", $1}'
    fi
 }
 
@@ -61,12 +89,22 @@ function prometheus-lib-get-latest-value() {
 #######################
    local metric="$1"
    local range="$2"
+   local curl_output=""
+   local jq_output=""
 
    if [ $# -lt 2 ]
    then
       echo "Too few parameters, need metric & range" >&2
    else
-      curl -s "${PROMETHEUS_API}/query?query=last_over_time(${metric}\[${range}\])" | jq -r '.data.result[0].value[1]'
+      #curl -s "${PROMETHEUS_API}/query?query=last_over_time(${metric}\[${range}\])" | jq -r '.data.result[0].value[1]'
+
+      curl_output=$(curl -s "${PROMETHEUS_API}/query?query=last_over_time(${metric}\[${range}\])")
+      prometheus-lib "curl_output" "$curl_output"
+
+      jq_output="$(echo "$curl_output" | jq -r '.data.result[0].value[1]')"
+      prometheus-lib "jq_output" "$jq_output"
+
+      echo "$jq_output"
    fi
 
 }
