a5a352dc846481ae3832acf0b46179d458431c86
pjp <paul@pearceful.net>
2024-02-17 05:44:04 +0000

    Reversed the logic for sending an email

--- bin/perform-speed-test.sh
+++ bin/perform-speed-test.sh
@@ -2,7 +2,8 @@
 
 curl -s localhost:9798/metrics | grep -v '^#' | grep 'load' | sed 's/_bits_per_second//' | awk '{ printf "%-25s %4dMb/s\n", $1, $2/1000000}' > ${1:-/tmp/speed.txt}
 
-if [ $# -gt 0 ]
+# Dont send an email if any parameter is passed
+if [ $# -eq 0 ]
 then
    $HOME/bin/mail-current-speedtest-results.sh
 fi
