4fb2fac7ee29ba47230ca8bb36c5df56458e7968
pjp <paul@pearceful.net>
2024-02-17 08:03:32 +0000

    Removed the dependency on a email script

--- bin/perform-speed-test.sh
+++ bin/perform-speed-test.sh
@@ -2,6 +2,18 @@
 
 export PATH="$HOME/bin:$PATH"
 
+#############################################
+# Set this to a script that will do something
+# interesting e.g. sent an email maybe
+#
+# POST_HOOK_SCRIPT=""
+
 curl -s localhost:9798/metrics | grep -v '^#' | grep 'load' | sed 's/_bits_per_second//' | awk '{ printf "%-25s %4dMb/s\n", $1, $2/1000000}' > ${1:-/tmp/speed.txt}
 
-mail-current-speedtest-results.sh
+###########################################
+# Check to see if a post hook was specified
+#
+if [ ! -z "$POST_HOOK_SCRIPT" ]
+then
+   $POST_HOOK_SCRIPT
+fi
