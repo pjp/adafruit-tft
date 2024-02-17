b00e366c4475cf58a5e062e275c5e942687806ea
pjp <paul@pearceful.net>
2024-02-17 04:33:49 +0000

    Added option to mail results after the test

--- bin/perform-speed-test.sh
+++ bin/perform-speed-test.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 curl -s localhost:9798/metrics | grep -v '^#' | grep 'load' | sed 's/_bits_per_second//' | awk '{ printf "%-25s %4dMb/s\n", $1, $2/1000000}' > ${1:-/tmp/speed.txt}
+
+if [ $# -gt 0 ]
+then
+   $HOME/bin/mail-current-speedtest-results.sh
+fi
