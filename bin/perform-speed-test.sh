1d9415e4f8f6852eb686bcaa581aa1d0f998122e
pjp <paul@pearceful.net>
2024-02-17 07:31:57 +0000

    Send an email as well

--- bin/perform-speed-test.sh
+++ bin/perform-speed-test.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
+export PATH="$HOME/bin:$PATH"
+
 curl -s localhost:9798/metrics | grep -v '^#' | grep 'load' | sed 's/_bits_per_second//' | awk '{ printf "%-25s %4dMb/s\n", $1, $2/1000000}' > ${1:-/tmp/speed.txt}
 
-# Dont send an email if any parameter is passed
-if [ $# -eq 0 ]
-then
-   $HOME/bin/mail-current-speedtest-results.sh
-fi
+mail-current-speedtest-results.sh
