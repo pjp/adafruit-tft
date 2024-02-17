766bb3575cf69fe1bec0d9b3b36573f64a77243d
pjp <paul@pearceful.net>
2024-02-17 08:06:45 +0000

    Slight code update

--- bin/perform-speed-test.sh
+++ bin/perform-speed-test.sh
@@ -6,7 +6,7 @@ export PATH="$HOME/bin:$PATH"
 # Set this to a script that will do something
 # interesting e.g. sent an email maybe
 #
-# POST_HOOK_SCRIPT=""
+POST_HOOK_SCRIPT=""
 
 curl -s localhost:9798/metrics | grep -v '^#' | grep 'load' | sed 's/_bits_per_second//' | awk '{ printf "%-25s %4dMb/s\n", $1, $2/1000000}' > ${1:-/tmp/speed.txt}
 
